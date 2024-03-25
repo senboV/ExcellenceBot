820e1d920663ba98f68491d6ba6304b6490b6eb0
SenboV <senbo.endr@gmail.com>
2024-03-24 21:28:28 -0400

    azure requires ssl connection to DB

--- utils/databaseHelper.js
+++ utils/databaseHelper.js
@@ -8,7 +8,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_ENDPOINT,
     dialect: 'postgres',
-    logging: false,
+    //logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true
+        }
+    },
 });
 
 // imported Database Models, using the existing database connection
